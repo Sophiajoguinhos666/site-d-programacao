7e997b165206eb07f91cf534f06d8b18bd7c8f97
Sophiajoguinhos666 <132074657+Sophiajoguinhos666@users.noreply.github.com>
2023-09-14 08:29:37 -0300

    ,,m

---
+++ Programação/main.js
@@ -0,0 +1,10 @@
+function Toca () {
+    document.querySelector('#som_tecla_toim').play();
+}
+const lista = document.querySelectorAll('.tecla');
+
+let Contador = 0;
+
+while (Contador <9) {
+    
+}
