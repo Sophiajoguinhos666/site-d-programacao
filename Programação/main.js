99c8c9b5dbd325f6ca0c8177af5f2e84e50a4735
Sophiajoguinhos666 <132074657+Sophiajoguinhos666@users.noreply.github.com>
2023-09-21 08:27:32 -0300

    gf

--- Programação/main.js
+++ Programação/main.js
@@ -1,5 +1,5 @@
 function Toca (idElementoAudio) {
-    document.querySelector('idElementoAudio').play();
+    document.querySelector(idElementoAudio).play();
 }
 const lista = document.querySelectorAll('.tecla');
 
