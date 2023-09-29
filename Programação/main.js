8309be03a2f7cdea0c9545a53e989dd8a3ba016d
Sophiajoguinhos666 <132074657+Sophiajoguinhos666@users.noreply.github.com>
2023-09-29 09:36:29 -0300

    gfgg

--- Programação/main.js
+++ Programação/main.js
@@ -8,7 +8,7 @@ let Contador = 0;
 
 while (Contador < lista.length) {
     lista [Contador].onclick = function() {
-        Toca('idAudio');
+        Toca(idAudio);
     }
     const classe = lista[Contador].classList[1];
     console.log(classe);    
