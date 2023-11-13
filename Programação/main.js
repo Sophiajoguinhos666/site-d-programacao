d9d881a7e322a5a157d0a549213a0816ce925ddd
Sophiajoguinhos666 <132074657+Sophiajoguinhos666@users.noreply.github.com>
2023-11-13 11:51:09 -0300

    gg

--- Programação/main.js
+++ Programação/main.js
@@ -31,3 +31,32 @@ for (let Contador = 0; Contador < lista.length;Contador++  ) {
     }
 }
 
+<script>
+        function calcular() {
+            var numero1 = parseFloat(document.getElementById("numero1").value);
+            var numero2 = parseFloat(document.getElementById("numero2").value);
+            var operador = document.getElementById("operador").value;
+            var resultado = 0;
+
+            switch (operador) {
+                case "+":
+                    resultado = numero1 + numero2;
+                    break;
+                case "-":
+                    resultado = numero1 - numero2;
+                    break;
+                case "*":
+                    resultado = numero1 * numero2;
+                    break;
+                case "/":
+                    resultado = numero1 / numero2;
+                    break;
+                case "**":
+                    resultado = numero1 ** numero2;
+                    break;
+            }
+
+            document.getElementById("resultado").innerHTML = "Resultado: " + resultado;
+        }
+        
+    </script>
